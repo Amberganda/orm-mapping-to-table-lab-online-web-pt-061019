03c4e13e95c66883ff7d15ff6f5b608e6b1b1813
Amber Taranto <ambzzzz@gmail.com>
2019-08-29 00:05:51 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -20,4 +20,15 @@ class Student
         SQL
         DB[:conn].execute(sql)
     end
+
+    def self.drop_table
+        sql = <<-SQL
+        DROP TABLE students (
+            id INTEGER PRIMARY KEY,
+            name TEXT,
+            grade INTEGER
+        )
+        SQL
+        DB[:conn].execute(sql)
+
 end
